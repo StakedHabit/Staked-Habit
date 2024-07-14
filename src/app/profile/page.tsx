c0f1496b8a147cc63c5d7e80e46e9994079d0bb8
gulshanpr <gulshankumarprasad04@gmail.com>
2024-07-14 09:46:51 +0530

    nav bar added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 function HomeProfile({data}: {data: any}) {
   return (
-    <div> 
-      <h1>{data}</h1>
+    <div>
+    
+    <div className="mx-10 my-5 flex items-end justify-end">
+        <nav className="flex items-end flex space-x-4">
+          <a href="/home" className="text-black">
+            Home
+          </a>
+          <a href="/leaderboard" className="text-black" >
+            Leaderboard
+          </a>
+          <a href="/profile" className="text-black" >
+            Profile
+          </a>
+        </nav>
+      </div>
     </div>
     
   )
